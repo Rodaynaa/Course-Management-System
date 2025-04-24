82ba7cbbe0d9b2538fb82840aa411e1c5197d6ca
Rodaynaa <roudymohammed2002@gmail.com>
2025-04-24 20:03:40 +0200

    adding vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const serverless = require('vercel');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const courseRoutes = require('./routes/courses');
+const courseRoutes = require('../routes/courses');
 
 dotenv.config();
 const app = express();
@@ -21,3 +22,6 @@ mongoose.connect(process.env.MONGO_URI, {
   app.listen(5000, () => console.log('Server running on port 5000'));
 })
 .catch(err => console.error(err));
+
+module.exports = app;
+module.exports.handler = serverless(app);
