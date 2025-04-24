316b2ebc2c1fe19f6ef0ec0499671b7484be53a6
Rodaynaa <roudymohammed2002@gmail.com>
2025-04-24 20:49:48 +0200

    fixing issue

--- api/index.js
+++ api/index.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const serverless = require('serverless-http');
 const courseRoutes = require('../routes/courses');
 
 dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/courses', courseRoutes);
 
-console.log(process.env.MONGO_URI);
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => {
-  console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on port 5000'));
-})
+.then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
 
-module.exports = app;
+module.exports.handler = serverless(app);
